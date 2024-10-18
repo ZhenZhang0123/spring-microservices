1d8b8bb4de6ce03ccfb828d803abe54f8e147e38
zhenzhang <zhen.zhang0123@gmail.com>
2024-10-18 17:50:38 -0400

    Connecting REST API to H2 using JPA and Hibernate - An Overview, Creating User Entity and some test data

--- src/main/java/com/zhenzhang0123/spring/springmicroservices/entity/User.java
+++ src/main/java/com/zhenzhang0123/spring/springmicroservices/entity/User.java
@@ -1,13 +1,22 @@
 package com.zhenzhang0123.spring.springmicroservices.entity;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
 import jakarta.validation.constraints.Past;
 import jakarta.validation.constraints.Size;
 
 import java.time.LocalDate;
 
+@Entity(name = "user_details")
 public class User {
+    protected User() {
 
+    }
+
+    @Id
+    @GeneratedValue
     private Integer id;
 
     @Size(min=2, message = "Name should have atleast 2 characters")
