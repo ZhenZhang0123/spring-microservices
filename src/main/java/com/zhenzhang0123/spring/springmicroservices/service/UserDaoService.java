a61491c0d9e78428f8a6708cb06dc0f2abb9d2b8
zhenzhang <zhen.zhang0123@gmail.com>
2024-10-18 00:20:17 -0400

    Implementing DELETE Method to delete a User Resource

--- src/main/java/com/zhenzhang0123/spring/springmicroservices/service/UserDaoService.java
+++ src/main/java/com/zhenzhang0123/spring/springmicroservices/service/UserDaoService.java
@@ -38,4 +38,8 @@ public class UserDaoService {
         return users.stream().filter(predicate).findFirst().get();
     }
 
+    public void deleteById(int id) {
+        Predicate<? super User> predicate = user -> user.getId().equals(id);
+        users.removeIf(predicate);
+    }
 }
