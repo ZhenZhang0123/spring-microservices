6276e58cd6e72b0c317fc751a78c41fc84477bfe
zhenzhang <zhen.zhang0123@gmail.com>
2024-10-18 13:50:28 -0400

    Versioning REST API - URI Versioning, Versioning REST API - Request Param, Header and Content Negotiation

---
+++ src/main/java/com/zhenzhang0123/spring/springmicroservices/controller/versioning/PersonV2.java
@@ -0,0 +1,20 @@
+package com.zhenzhang0123.spring.springmicroservices.controller.versioning;
+
+public class PersonV2 {
+    private Name name;
+
+    public PersonV2(Name name) {
+        super();
+        this.name = name;
+    }
+
+    public Name getName() {
+        return name;
+    }
+
+    @Override
+    public String toString() {
+        return "PersonV2 [name=" + name + "]";
+    }
+
+}
